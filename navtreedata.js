566f4a4061fb3f47f068978da9a2b810c28ddab3
uyjulian <uyjulian@users.noreply.github.com>
2024-09-24 15:23:50 +0000

    deploy: e8ae9b64152e1989f3525e0fc548a54275e74e10

---
+++ navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_2kernel_2_r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_2rpc_2remote_2remote.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"atahw_8h.html#a414747b879d890c1df55ccbd4208cf1ca8b2c6c7e319818c77569ecb3e7b0f981",
+"audsrv_8c.html#addc7abfb9fc4d953e5e6bd5014948a3e",
+"dir_0134b0376fbe77322088b496fb368f9b.html",
+"dir_7ce330b52aeb23b4ce737d3f4b393244.html",
+"dir_fe7329d8b89241d59f89f45fd75d2e77.html",
+"ee_2network_2tcpip_2include_2ps2ip_8h.html#a3e1f3b6cd60ffe63f3757d374399d9f4",
+"ee__regs_8h.html#ad3fc1d4168a047597df14d994426363f",
+"file_xio_8h.html#structfxio__lseek64__return__pkt",
+"globals_func_a.html",
+"group__packet2__types.html#a4862c9e86250ca2ec5e62758c8318efc",
+"gs__psm_8h.html#a3bae67a3ae582848860b01d1da769a44",
+"ioman_8c_source.html",
+"iop_2system_2eesync_2src_2irx__imports_8h_source.html",
+"kernel_8h.html#structt__ee__thread",
+"libcdvd_8c.html#aa5b446173906c8e68694103114f775ed",
+"libgs_8h.html#ac78544c22ca0f5069ed1b7250259fd7b",
+"libkbd_8c.html#a5a274b5f9886aa3df84b373902bd6b49",
+"libpad_8c.html#ac3445e7b01e2670760520b2987c701de",
+"misc__hdck_8c_source.html",
+"poll_8c_source.html",
+"ps2ip__rpc_8h.html#structret__pkt",
+"s__stsa_8c_source.html",
+"smapregs_8h.html#a760588abb97d68daa2b47ef0f9d216a5",
+"sysclib__determine__base_8h_source.html",
+"usbd_8h.html#aed14c3f2e96e50522fbf2cdc32e16e39"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
